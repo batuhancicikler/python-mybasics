69b6f5b49626e09b93c8a5147650f3f2ac2fec49
batuhan <batuhan.cicikler@gmail.com>
2019-01-30 22:36:04 +0300

    Kart Oyunu Örneği (Class çalışması)

--- örnekler2.py
+++ örnekler2.py
@@ -150,6 +150,126 @@ def fibo2(n):
         a, b = b, a + b
     print()
 
+#%% Algoritma Analizleri
+    
+import time
+
+def sumOf(n):
+    start = time.time()
+    theSum = 0
+    for i in range(0,n + 1):
+        theSum += i
+    end = time.time()
+    
+    return theSum, end - start
+
+def sumOf2(n):
+    start = time.time()
+    toplam = (n * (n + 1)) / 2
+    end = time.time()
+    
+    return toplam, end - start
+
+print("Sum1 %d zamanı da %10.7f saniye" %(sumOf(1000000)))
+print("Sum2 %d zamanı da %10.7f saniye" %(sumOf2(1000000)))
+
+#%% Kart oyunu
+
+from enum import Enum
+from enum import IntEnum
+from random import *
+
+
+full_deste = []
+gecici_deste = []
+player_kart = []
+player2_kart = []
+
+    # kart enum u, deste için
+class Kart(IntEnum):
+    İKİ = 2
+    ÜÇ = 3
+    DÖRT = 4
+    BEŞ = 5
+    ALTI = 6
+    YEDİ = 7
+    SEKİZ = 8
+    DOKUZ = 9
+    ON = 10
+    JOKER = 11
+    KIZ = 12
+    KRAL = 13
+    AS = 14
+
+    # suit enum u
+class Suit(Enum):
+    MAÇALAR = "maçalar"
+    SİNEKLER = "sinekler"
+    KUPALAR = "kupalar"
+    KAROLAR = "karolar"
+    
+
+    # kart oyunu için bilgi tutacak class
+class KartOyunu:
+    def __init__(self, kart_value, kart_suit):
+        self.kart = kart_value
+        self.suit = kart_suit
+        
+        # tüm deste kartlarıyla ilgilenmesi için gerekli fonksiyon
+def deste_olustur():
+    for suit in Suit:
+        for kart in Kart:
+            full_deste.append(KartOyunu(Kart(kart), Suit(suit)))
+    return full_deste
+    
+
+    # desteden tek kart çekme
+def draw_kart(deste):
+    rand_kart = randint(0, len(deste) - 1)
+    return deste.pop(rand_kart)     # çekilen kartı return edip, desteden çıkartıyor
+    
+        
+    # oyuncuların destelerine, geçici desteden kağıtları dağıtıyor
+def halfdeal():
+    while(len(gecici_deste) > 0):
+        player_kart.append(draw_kart(gecici_deste))
+        player2_kart.append(draw_kart(gecici_deste))
+
+deste_olustur()
+gecici_deste = list(full_deste)
+halfdeal()
+
+for i in range(0, len(player_kart)):
+    if(player_kart[i].kart > player2_kart[i].kart):
+        print("Player 1 eli kazandı !", player_kart[i].kart)
+        print("Player 2 eli kaybetti !", player2_kart[i].kart)
+        
+    if(player2_kart[i].kart > player_kart[i].kart):
+        print("Player 2 eli kazandı !", player2_kart[i].kart)
+        print("Player 1 eli kaybetti !", player_kart[i].kart)
+    else:
+        print("Kazanan Yok")
+        
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
