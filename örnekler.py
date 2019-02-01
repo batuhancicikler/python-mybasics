a109ba8d1c09694890ac1fd979365e3280b40191
batuhan <batuhan.cicikler@gmail.com>
2019-02-02 01:47:54 +0300

    numpy softmax örneği --- deneme

--- örnekler.py
+++ örnekler.py
@@ -364,10 +364,22 @@ def sırala(liste):
                 liste[i + 1] = temp
     return liste
 
+#%%
+    
+import numpy as np
 
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis = 0)
 
-        
+skorlar = [3.0, 1.0, 0.2]
+print(softmax(skorlar))
 
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+skorlar = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(skorlar).T, linewidth=2)
+plt.show()
     
     
     
